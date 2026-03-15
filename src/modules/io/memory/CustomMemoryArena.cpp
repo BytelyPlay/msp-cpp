bc3c477cc7bd57f6d4e5005e0ed370b1d242a5fb
BytelyPlay <universalnebula2024@outlook.com>
2026-03-15 08:54:43 +0100

    Memory arena seems to work

--- src/modules/io/memory/CustomMemoryArena.cpp
+++ src/modules/io/memory/CustomMemoryArena.cpp
@@ -2,6 +2,8 @@ module;
 #include <cstdlib>
 #include <memory>
 #include <unordered_map>
+#include <vector>
+
 module CustomMemoryArena;
 
 // PRIVATE
@@ -10,8 +12,13 @@ bool CustomMemoryArena::Block::overlaps(Block b)
     auto intPtr = reinterpret_cast<uintptr_t>(ptr);
     auto givenIntPtr = reinterpret_cast<uintptr_t>(b.ptr);
 
-    return intPtr + size <= givenIntPtr + b.size
-        && givenIntPtr >= intPtr;
+    auto startA = intPtr;
+    auto endA = intPtr + size;
+
+    auto startB = givenIntPtr;
+    auto endB = givenIntPtr + b.size;
+
+    return startA < endB && startB < endA;
 }
 
 // PUBLIC
@@ -19,7 +26,7 @@ CustomMemoryArena::CustomMemoryArena(size_t initialAmount)
 {
     void* initialBlock = malloc(initialAmount);
     if (!initialBlock)
-        return;
+        throw std::bad_alloc();
     Block block {
         initialBlock,
         initialAmount
@@ -39,15 +46,24 @@ void* CustomMemoryArena::alloc(size_t amount, size_t alignment)
 {
     for (Block b : memoryBlocks)
     {
-        if (b.size < amount)
+        // Could this code be worse...
+        if (b.size >= amount)
         {
+            size_t sizeOfBlock = b.size;
+
             void* alignedPtr = std::align(alignment,
                 amount,
                 b.ptr,
-                b.size);
+                sizeOfBlock
+                );
+            auto alignedIntPtr = reinterpret_cast<
+                uintptr_t>(alignedPtr);
+            auto alignedBlockPtr = reinterpret_cast<
+                uintptr_t>(b.ptr);
             if (!alignedPtr)
                 return nullptr;
             bool foundAllocation = true;
+
             while (!canGiveOut(
                 Block {
                     alignedPtr,
@@ -55,12 +71,14 @@ void* CustomMemoryArena::alloc(size_t amount, size_t alignment)
                 })
             )
             {
-                alignedPtr += alignment;
-                if (alignedPtr + amount > b.ptr + b.size)
+                alignedIntPtr += alignment;
+
+                if (alignedIntPtr + amount > alignedBlockPtr + b.size)
                 {
                     foundAllocation = false;
                     break;
                 }
+                alignedPtr = reinterpret_cast<void*>(alignedIntPtr);
             }
             if (!foundAllocation) continue;
 
@@ -98,9 +116,9 @@ bool CustomMemoryArena::canGiveOut(Block b)
 {
     for (Block b1 : blocksGivenOut)
     {
-        if (b1.overlaps(b)) return true;
+        if (b1.overlaps(b)) return false;
     }
-    return false;
+    return true;
 }
 
 // PRIVATE
--- test/sources/main.cpp
+++ test/sources/main.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cstdint>
 
 import MinecraftProtocol;
 import MinecraftServer;
@@ -6,14 +7,23 @@ import Logger;
 import EndiannessUtils;
 import TypedInputStream;
 import TypedOutputStream;
-
+import CustomMemoryArena;
 int main() {
+    CustomMemoryArena arena(1024 * 1024);
+    void* mem = arena.alloc(1024, 32);
+
+    auto* memAsBytes = reinterpret_cast<unsigned char*>(mem);
+    memAsBytes[1024] = 59;
+    Logger::debug(std::to_string(memAsBytes[1024]));
+
+    return 0;
+
     Logger::debug("Is Big Endian: " +
         std::string((EndiannessUtils::isBigEndian() ? "Yes" : "No")));
 
     TypedOutputStream out;
     out << 1234567;
-    
+
     MinecraftProtocol protocol(4);
     protocol.init();
     MinecraftServer server("127.0.0.1", 24213, protocol);
