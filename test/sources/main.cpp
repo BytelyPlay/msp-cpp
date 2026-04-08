f1f825819cae744b97b08b34a5304bbc6c722bc6
BytelyPlay <universalnebula2024@outlook.com>
2026-04-08 14:44:00 +0200

    After figuring out the memory leak was temporary code in main.cpp I thought to commit

--- test/sources/main.cpp
+++ test/sources/main.cpp
@@ -20,36 +20,6 @@ import UUIDPacketCodec;
 import VarIntPacketCodec;
 
 int main() {
-    VarIntPacketCodec& varIntCodec = VarIntPacketCodec::getInstance();
-
-    for (int i = 0; i < 1000; i++)
-    {
-        uint consumed;
-
-        std::vector<unsigned char> varInt = varIntCodec.serialize(i);
-        if (varIntCodec.deserialize(varInt, consumed) != i)
-            Logger::warn(
-                std::format(
-                    "VarIntPacketCodec has something wrong with it: "
-                    "{} produced: {}", i,
-                    varIntCodec.deserialize(varInt, consumed)
-                    ));
-    }
-    Logger::debug("Is Big Endian: " +
-        std::string((EndiannessUtils::isBigEndian() ? "Yes" : "No")));
-
-    UUID uuid = generateUUID();
-
-    std::string hexBytes;
-    for (unsigned char& newByte : uuid.uuid)
-    {
-        hexBytes += std::format("{:02X} ", newByte);
-    }
-    Logger::debug(hexBytes + "         " + uuid.toString());
-
-    TypedOutputStream out;
-    out << 1234567;
-
     MinecraftProtocol protocol(4);
     protocol.init();
     MinecraftServer server("127.0.0.1", 24213, protocol);
