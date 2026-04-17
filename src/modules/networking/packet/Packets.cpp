49dcdefd11f24594be303877c44012c6f65d7451
BytelyPlay <universalnebula2024@outlook.com>
2026-04-17 19:54:31 +0200

    I now properly handle exceptions.

--- src/modules/networking/packet/Packets.cpp
+++ src/modules/networking/packet/Packets.cpp
@@ -13,6 +13,7 @@ import PacketC2S;
 import MinecraftServer;
 import MinecraftProtocol;
 import MinecraftClient;
+import IOException;
 
 // Packets::PacketRegister
 // PUBLIC
@@ -23,41 +24,52 @@ Packets::PacketsRegister& Packets::PacketsRegister::getInstance()
     return packetsRegister;
 }
 // PUBLIC
-// TODO: Handle exceptions.
 void Packets::PacketsRegister::receivedPacket(
     std::vector<unsigned char> data,
     MinecraftServer& server,
     MinecraftProtocol& protocol,
     MinecraftClient& client)
 {
-    TypedInputStream in = TypedInputStream(data.data(),
-        data.data() + data.size());
-    int id = VarIntPacketCodec::getInstance().deserialize(in);
-
-    // TODO: Replace with std::unordered_map
-    for (auto typeWrapper : types)
+    try
     {
-        PacketType& type = typeWrapper.get();
+        TypedInputStream in = TypedInputStream(data.data(),
+            data.data() + data.size());
+        int id = VarIntPacketCodec::getInstance().deserialize(in);
 
-        if (type.getPacketID() == id &&
-            type.getPhase() ==
-            client.getPhase() &&
-            type.isC2S())
+        // TODO: Replace with std::unordered_map
+        for (auto typeWrapper : types)
         {
-            // Type is a PacketTypeC2S
-            auto& c2sType = static_cast<PacketTypeC2S&>(type);
+            PacketType& type = typeWrapper.get();
 
-            c2sType.deserializeAndCall(
-                in,
-                server,
-                protocol,
-                client
-            );
-            return;
+            if (type.getPacketID() == id &&
+                type.getPhase() ==
+                client.getPhase() &&
+                type.isC2S())
+            {
+                // Type is a PacketTypeC2S
+                auto& c2sType = static_cast<PacketTypeC2S&>(type);
+
+                c2sType.deserializeAndCall(
+                    in,
+                    server,
+                    protocol,
+                    client
+                );
+                return;
+            }
         }
+        Logger::warn("No Packet Type found for packet ID: " +
+            std::to_string(id) + " in phase: " + std::to_string(client.getPhase()));
+    } catch (IOException& e)
+    {
+        Logger::warn(std::string(
+          "IOException, "
+          "this could be a simple failure "
+          "in the deserialization of the packet "
+          "or a deeper problem, the former is "
+          "more likely, what: "
+        ) + e.what());
     }
-    Logger::warn("No Packet Type found for packet ID: " +
-        std::to_string(id) + " in phase: " + std::to_string(client.getPhase()));
 }
 // PUBLIC
 
